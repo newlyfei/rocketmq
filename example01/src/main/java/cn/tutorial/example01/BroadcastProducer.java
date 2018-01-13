531f100a402c8964719f3b5745ea3689108abe71
lyf <liyanfei@ds.gome.com.cn>
2018-01-13 17:18:31 +0800

    Initial commit

---
+++ example01/src/main/java/cn/tutorial/example01/BroadcastProducer.java
@@ -0,0 +1,24 @@
+package cn.tutorial.example01;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+public class BroadcastProducer {
+    public static void main(String[] args) throws Exception {
+        DefaultMQProducer producer = new DefaultMQProducer("ProducerGroupName");
+        producer.setNamesrvAddr("192.168.56.11:9876");
+        producer.start();
+
+        for (int i = 0; i < 10; i++){
+            Message msg = new Message("TopicTest",
+                    "TagA",
+                    "OrderID188",
+                    "Hello world".getBytes(RemotingHelper.DEFAULT_CHARSET));
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult);
+        }
+        producer.shutdown();
+    }
+}
